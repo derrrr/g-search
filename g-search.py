46749177727166a4bdec00932e79c6832c3b8e2c
Unknown <102302031@nccu.edu.tw>
2018-08-07 23:48:11 +0800

    Fix search rule for url and title

--- g-search.py
+++ g-search.py
@@ -213,8 +213,8 @@ class G_search:
             rank = 1
             self.search = 0
             for s_res in soup_no_ad.find(id="ires").find_all(class_="g"):
-                title_part = re.sub("\s*\.*\\n\s*", "", str(s_res.a.string))[:title_slice]
-                if unquote(target[2]) in unquote(s_res.a["href"]) or (title_part in target[1] and urlparse(unquote(target[2])).hostname in urlparse(unquote(s_res.a["href"])).hostname):
+                title_part = re.sub("\s*\.*\\n\s*", "", str(s_res.a.string))
+                if unquote(target[2]) == unquote(s_res.a["href"]) or (target[1][:title_slice] == title_part[:title_slice] and urlparse(unquote(target[2])).hostname == urlparse(unquote(s_res.a["href"])).hostname):
                     s_res.find(class_="rc")["style"] = "border-width:2px; border-style:solid; border-color:red; padding:1px;"
                     message = "關鍵字: {} {}\t在 第{}頁 第{}個 找到\n{}".format(\
                         key_word[0], key_word[1], page_count, rank, target[2])
